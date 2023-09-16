32b6c0d72e56bd10be8a7e10a633f6827adac87d
goosewood <goosewood@protonmail.com>
2023-09-16 02:16:14 -0400

    taking files as inputs

--- main.py
+++ main.py
@@ -12,42 +12,42 @@ Takes list arguments ingedients, allergies, medication
 
 import json, os, sys, subprocess
 
-if __name__ == "__main__":
-
-      ing = sys.argv[1]
-      allergy = sys.argv[2]
-      med = sys.argv[3]
-
-    for i in range(len(ing)):
-        if isAllergen(ing[i]):
-            print(str(ing[i]) + "is an allergen")
-
-        for m in range(len(med)):
-            if hasDrugInteraction(ing[i]):
-                print(str(ing[i]) + "has drug interaction with" + str(med[m]))
-                # add chatGPT interaction + drugs.com
-
-
-
-
-    except Error as e:
-        print()
-        print(("Error --" + e.msg))
-        exit()
-    except KeyboardInterrupt:
-        print("\nExiting...")
-        exit()
-
-
-
 
 def isAllergen(food):
     # check food allergen list, retuns T/F
-    return True
-    return False
+    with open('allergens.txt') as in_file:
+        for line in in_file:
+            line = line.strip()
+            if line == food:
+                return True
+        return False
 
 def hasDrugInteraction(food):
     # returns T/F
 
     return False
     return True
+
+ing = []
+
+with open(sys.argv[1]) as in_file:
+        for line in in_file:
+            line = line.strip()
+            ing.append(line)
+
+
+if __name__ == "__main__":
+#    ing = sys.argv[1]
+    print(ing)
+    allergy = sys.argv[2]
+    med = sys.argv[3]
+
+    for i in range(len(ing)):
+        if isAllergen(ing[i]):
+            print(str(ing[i]) + " is an allergen")
+
+        for m in range(len(med)):
+            if hasDrugInteraction(ing[i]):
+                print(str(ing[i]) + "has drug interaction with" + str(med[m]))
+                # add chatGPT interaction + drugs.com
+
